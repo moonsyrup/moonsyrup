be4df4af991a412e130d503bc83c4db587056946
Javier Bullrich <javier.bullrich@bitwala.com>
2022-10-27 19:00:54 +0200

    fix(jquery): removed jquery
    Removed jquery and made code work with native js

--- js/init.js
+++ js/init.js
@@ -1,11 +1,12 @@
 import { ethers } from "./ethers-5.1.esm.min.js";
 
 const setBlockNumber = (block) => {
-    $("#block").text(block);
-    $("#block").attr("href", `https://etherscan.io/block/${block}`)
+    const blockCounter = document.getElementById("block");
+    blockCounter.innerHTML = block;
+    blockCounter.href = `https://etherscan.io/block/${block}`;
 }
 
-jQuery(document).ready(function($) {
+document.addEventListener("DOMContentLoaded", function() {
     const eth = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/961f0bb451ce43868ef45e2098008e9f");
 
     eth.getBlockNumber().then((block) => {
